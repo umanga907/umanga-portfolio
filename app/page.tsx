2c7d1b69ae59dcc7a7201cc978d2cd9194cef784
umanga907 <umanga.shrestha@portpro.io>
2023-08-28 10:44:53 +0545

    removed pages and added dummy content

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">Hi, my name is Umanga, I'm Lead UI/UX Engineer at Portpro. I'm deeply involved in impactful work at Portpro while actively developing my portfolio.</h2>
+        <h2 className="text-sm text-zinc-500 ">Hi, my name is Umanga, I'm Lead UI/UX Engineer at Portpro. I'm deeply involved in impactful work at Portpro while trying to slowly build up my portfolio.</h2>
       </div>
     </div>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,9 +20,9 @@ export default async function ProjectsPage() {
 		return acc;
 	}, {} as Record<string, number>);
 
-	const featured = allProjects.find((project) => project.slug === "unkey")!;
-	const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-	const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+	const featured = allProjects.find((project) => project.slug === "project-x")!;
+	const top2 = allProjects.find((project) => project.slug === "project-y")!;
+	const top3 = allProjects.find((project) => project.slug === "project-z")!;
 	const sorted = allProjects
 		.filter((p) => p.published)
 		.filter(
@@ -46,7 +46,7 @@ export default async function ProjectsPage() {
 						Projects
 					</h2>
 					<p className="mt-4 text-zinc-400">
-						Some of the projects are from work and some are on my own time.
+						This is just a dummy area, will try to fill up this section slowly.
 					</p>
 				</div>
 				<div className="w-full h-px bg-zinc-800" />
